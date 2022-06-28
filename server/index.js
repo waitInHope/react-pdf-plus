5a2d1485cdbc2e1997ceb407442a6c1f84b4e9a2
zhangzhendong.zzd <zhangzhendong.zzd@bytedance.com>
2022-06-28 14:49:26 +0800

    feat: 实现server读取并返回pdf文件到浏览器，页面通过react-pdf正常展示

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ console.log('开始初始化服务端')
 
 const Koa = require('koa');
 const Router = require('koa-router');
+const fs = require('fs');
+const path = require('path');
 
 const app = new Koa();
 const router = new Router();
@@ -10,11 +12,17 @@ const router = new Router();
 const port = 9090;
 
 router.get('/pdf', (ctx, next) => {
-    console.log('收到客户端的请求信息')
-    ctx.body = {
-        code: 200,
-        data: 'hello word'
-    }
+    console.log('收到客户端的请求信息');
+    // 读取本地pdf文件并返回给浏览器
+    let url = path.join(__dirname, '../assets/2022年留杭承诺书.pdf');
+    console.log(`文件的路径${url}`);
+    ctx.set('Content-Type', 'application/pdf');
+    let content = fs.readFileSync(url, 'binary');
+
+    const buf = Buffer.from(content, 'binary');
+    // const arrayBuf = new Int8Array(buf.data);
+    ctx.body = buf;
+    // ctx.set('Transfer-Encoding', 'chunked');
     next();
 })
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,54 @@
 
-import React, { useEffect } from "react";
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+// import axios from 'axios';
+import { Document, Page, pdfjs } from 'react-pdf';
+// import pdfworkerSrc from 'pdfjs-dist/build/pdf.worker.entry.js';
+// pdfjs.GlobalWorkerOptions.workerSrc = pdfworkerSrc;
+
+import './index.css'
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cat.net/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Home = React.memo(() => {
     const api = '/api/pdf';
 
+    // let [content, setContent] = useState('');
+    let [pageNum, setPageNum] = useState(0);
+
     useEffect(() => {
         // 调用代理接口获取数据
-        axios.get(api).then((res) => {
-            console.log('请求代理接口成功');
-        }, (err) => {
-            console.log('请求代理接口失败');
-        })
-    }, [])
+        // axios.get(api).then((res) => {
+        //     console.log('请求代理接口成功');
+            
+        //     const content = res.data;
+        //     debugger
+        //     // let arrayBuf = new Uint8Array(buffer);
+        //     // let blob = new Blob([arrayBuf], {type: 'application/pdf'});
+        //     // debugger
+        //     // let reader = new FileReader();
+        //     // reader.readAsDataURL(blob);
+        //     // reader.onload = () => {
+        //     //     debugger
+        //     //     setContent(reader.result)
+        //     // }
+        //     setContent(content)
+        // }, (err) => {
+        //     console.log('请求代理接口失败');
+        // })
+    }, []);
+
+    const loadSuccessHandle = (page) => {
+        const pages = page?._pdfInfo?.numPages;
+        setPageNum(pages);
+    }
 
     return (
         <div>
-            调用的后端接口{api}
+            <p>调用的后端接口{api}</p>
+            <div>
+                <Document file={'/api/pdf'} onLoadSuccess={loadSuccessHandle}>
+                    { pageNum ? new Array(pageNum).fill('').map((n, index) => <Page key={index} pageNumber={index + 1} className='pdfPageStyle' />) : ''}
+                </Document>
+            </div>
         </div>
     )
 })
